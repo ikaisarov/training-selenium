4e19a06f2fca7adfa23c9b24c7988d41a21a2e1c
ikaisarov <iva_kaisarinen@mail.ru>
2018-01-12 01:41:58 +0300

    LoginLitecart

---
+++ src/test/java/SecondLesson/LoginLitecart.java
@@ -0,0 +1,30 @@
+package SecondLesson;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginLitecart {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void myTestExample () {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+    @After
+    public void stop() {
+        driver.quit();
+        driver=null;
+    }
+}
